94ca67ef4e7a0f3cffcc7589f975980bb2e123ce
TheBlueFlame121 <pranshukumar27@gmail.com>
2022-03-02 10:01:59 +0530

    Fix legendre_symbol and add tonelli_shanks

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from math import isqrt
+from random import randint
 
 def valuation(n:int, i:int) -> int:
     v = 0
@@ -13,7 +14,36 @@ def legendre_symbol(n:int, p:int) -> int:
     elif pow(n, (p-1)//2, p) == 1:
         return 1
     else:
-        return 0
+        return -1
 
 def is_square(n:int) -> bool:
     return isqrt(n)**2 == n
+
+def tonelli_shanks(n:int, p:int) -> tuple[int, int]:
+    assert legendre_symbol(n, p) == 1, "N is not a quadratic residue"
+    pm1 = p-1
+    S = valuation(pm1, 2)
+    Q = pm1//(2**S)
+    z = randint(1, p)
+    while legendre_symbol(z, p) != -1:
+        z = randint(1, p)
+    M = S
+    c = pow(z, Q, p)
+    t = pow(n, Q, p)
+    R = pow(n, (Q+1)//2, p)
+    while True:
+        if t==0:
+            return 0, 0
+        if t == 1:
+            return R, p-R
+        i = 1
+        tsq = pow(t, 2, p)
+        while tsq != 1:
+            tsq = pow(tsq, 2, p)
+            i+=1
+        b = pow(c, pow(2, M-i-1, p-1), p)
+        R= (R*b)%p
+        t = (t*b*b)%p
+        c = (b*b)%p
+        M = i
+
